6f4073a6d8eb596afd8d008f7091f6853e2e41c1
YSRKEN <ysr.ken@gmail.com>
2019-08-06 17:20:47 +0900

    [add] 設定ファイルを追加した

--- focal-length-simulator/src/App.tsx
+++ focal-length-simulator/src/App.tsx
@@ -5,6 +5,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 import useStore from './ApplicationStore';
 import StateContext from './context';
 import CalcForm from './CalcForm';
+import { VERSION, TWITTER_URL, GITHUB_URL } from './env';
 
 const App: React.FC = () => {
   const context = useStore();
@@ -15,6 +16,12 @@ const App: React.FC = () => {
         <Row>
           <Col className="mx-auto my-3" xs={12} md={6}>
             <h2 className="text-center">画角シミュレーター</h2>
+            <div className="text-center">
+              <span>
+                Ver.{VERSION} <a href={TWITTER_URL}>作者Twitter</a>{' '}
+                <a href={GITHUB_URL}>GitHub</a>
+              </span>
+            </div>
             <CalcForm />
           </Col>
         </Row>
